7111dfcfe639c279ec0b4e154ddeef633c095fc0
glitchwizard <glitchwizard@gmail.com>
2019-01-07 22:18:12 -0800

    Passed triangle constructor test

--- spec/triangle-spec.js
+++ spec/triangle-spec.js
@@ -1,6 +1,11 @@
+import { Triangle } from "./../js/triangle.js";
+
 describe('Triangle', function() {
 
     it('should test whether a Triangle has three sides', function() {
-        //Test content will go here.
+        var triangle = new Triangle(3,4,5);
+        expect(triangle.side1).toEqual(3);
+        expect(triangle.side2).toEqual(4);
+        expect(triangle.side3).not.toEqual(6);
     });
 });
